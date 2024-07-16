d55c01322f900b1144bfd38ade6d020984acf06a
Manuel Nuñez <89217597+mnunez-newcom@users.noreply.github.com>
2024-07-16 17:39:07 +0000

    revert

--- models.py
+++ models.py
@@ -8,25 +8,18 @@ from odoo.exceptions import ValidationError
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    # @api.depends('company_id')
-    # def _compute_currency_id(self):
-    #     for template in self:
-    #         template.currency_id = self.env.ref('base.USD')
-    #         template.cost_currency_id = self.env.ref('base.USD')
+    @api.depends('company_id')
+    def _compute_currency_id(self):
+        for template in self:
+            template.currency_id = self.env.ref('base.USD')
+            template.cost_currency_id = self.env.ref('base.USD')
 
     currency_id = fields.Many2one(
             'res.currency','Currency',
-            # compute='_compute_currency_id',
-            # inverse='_inverse_currency_id',
-            )
+            compute='_compute_currency_id',inverse='_inverse_currency_id')
     cost_currency_id = fields.Many2one(
             'res.currency','Currency',
-            # compute='_compute_currency_id',
-            # inverse='_inverse_currency_id'
-            )
-    currency_id_test = fields.Many2one(
-            'res.currency','Currency',
-            )
+            compute='_compute_currency_id',inverse='_inverse_currency_id')
 
     def _inverse_currency_id(self):
         pass
